de23519ae14b5824b2642680e1b7329cfca476db
Telemething <mark@telemething.com>
2020-07-18 10:09:50 -0700

    Added demo method which shows how to use the TransportUdp class

--- Assets/SpiritGuide/TelemTransport.cs
+++ Assets/SpiritGuide/TelemTransport.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿//*****************************************************************************
+//* File: TelemTransport.cs
+//* Author: Mark West (markbrianwest@hotmail.com, mark.west@microsoft.com)
+//* Project: SpirtGuide (Microsoft Hackathon 2020)
+//* Description: Class to send UDP bropadcast packets
+//*****************************************************************************
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Diagnostics;
@@ -14,8 +21,73 @@ using Newtonsoft.Json.Converters;
 //* 1) In Unity, import the 'JSON.NET for Unity' asset.
 //* 2) If the solution refers to other projects which themselves refer to
 //*    Newtonsoft.JSON, then they must be also refer to 'JSON.NET for Unity'.
-//* 3) To do that, simply refer them to refer to:
+//* 3) To do that, simply refer them to:
 //*    "..\Assets\JsonDotNet\Assemblies\Windows\Newtonsoft.Json.dll"
+//*****************************************************************************
+
+//*****************************************************************************
+//* Base class to add JSON serialization to inheriter
+//*****************************************************************************
+
+class Message
+{
+    //*********************************************************************
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to
+    /// </summary>
+    /// <returns></returns>
+    //*********************************************************************
+    public string Serialize()
+    {
+        var settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+        //settings.Converters.Add(new StringEnumConverter(new CamelCaseNamingStrategy()));
+        settings.Converters.Add(new StringEnumConverter(true));
+        return JsonConvert.SerializeObject(this, settings);
+    }
+
+    //*********************************************************************
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    //*********************************************************************
+    public static SampleClass DeSerialize(string message)
+    {
+        var settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            MissingMemberHandling = MissingMemberHandling.Ignore
+        };
+        settings.Converters.Add(new StringEnumConverter(true));
+
+        SampleClass ret = null;
+
+        try
+        {
+            ret = JsonConvert.DeserializeObject<SampleClass>(message, settings);
+        }
+        catch (System.Exception ex)
+        {
+            //T1.CLogger.LogThis("DeSerialize() " + message);
+        }
+
+        return ret;
+    }
+}
+
+//*****************************************************************************
+//* Sample class to demo serialization
+//*****************************************************************************
+
+class SampleClass : Message
+{
+    public string stringData = "Don't surround yourself with yourself, move on back two squares.";
+    public int intData = 255;
+}
+
+//*****************************************************************************
+//* Class to send UDP bropadcast packets
 //*****************************************************************************
 
 class TransportUdp
@@ -47,6 +119,20 @@ class TransportUdp
 
     //*********************************************************************
     /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to
+    /// </summary>
+    /// <returns></returns>
+    //*********************************************************************
+    public string Serialize()
+    {
+        var settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
+        //settings.Converters.Add(new StringEnumConverter(new CamelCaseNamingStrategy()));
+        settings.Converters.Add(new StringEnumConverter(true));
+        return JsonConvert.SerializeObject(this, settings);
+    }
+
+    //*********************************************************************
+    /// <summary>
     /// Send bytes over the socket
     /// </summary>
     /// <param name="message"></param>
@@ -58,5 +144,62 @@ class TransportUdp
         var bytesSent = sock.SendTo(message, endpoint);
         return true;
     }
+
+    //*********************************************************************
+    /// <summary>
+    /// Send string over the socket
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    //*********************************************************************
+
+    public async Task<bool> Send(string message)
+    {
+        var bytesSent = sock.SendTo(Encoding.ASCII.GetBytes(message), endpoint);
+        return true;
+    }
+
+    //*********************************************************************
+    /// <summary>
+    /// Send string over the socket
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    //*********************************************************************
+
+    public async Task<bool> Send(Message message)
+    {
+        var bytesSent = sock.SendTo(Encoding.ASCII.GetBytes(message.Serialize()), endpoint);
+        return true;
+    }
+
+    //*********************************************************************
+    /// <summary>
+    /// Demo to send packets in three different ways
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    //*********************************************************************
+
+    public static bool Demo()
+    {
+        byte[] bites = { 0, 1, 2, 3, 4};
+        string sturing = "";
+        var sampleClass = new SampleClass();
+
+        var transport = new TransportUdp("192.168.1.255", 45678);
+
+        // Byte array
+        transport.Send(bites);
+
+        // String
+        transport.Send(sturing);
+
+        // Class (get serialized before transmission)
+        transport.Send(sampleClass);
+
+        return true;
+    }
+
 }
 
